cd373f3bc045bb7e9d093f37d2e75194893daa37
xiaoyu.liu <dizzle0722@163.com>
2020-12-16 13:48:02 +0800

    initialization

---
+++ Example/Pods/Target Support Files/XYCalendar/XYCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XYCalendar : NSObject
+@end
+@implementation PodsDummy_XYCalendar
+@end
---
+++ XYCalendar.swift
@@ -0,0 +1,12 @@
+//
+//  XYCalendar.swift
+//  XYCalendar
+//
+//  Created by lxy on 2020/12/16.
+//
+
+import Foundation
+
+class XYCalendar {
+    
+}
